e72b76ffdbcd0b9651bb50bc74a271f9423ca214
Polaco88h <r.polaco18@gmail.com>
2025-02-10 22:28:22 -0300

    ultimo paso

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // recibimos el nombre y lo metemos en un array 
-//let frutas = ["Manzana", "Banana"];
- 
+let amigos = [];  //Declaro el array
 
-let amigos = [];                 //Declaro el array
+function agregarAmigo() {
+    let campo = document.getElementById("amigo");  // Selecciono el campo del cual quiero obtener el contenido
+    let amigo = campo.value.trim();               // Obtengo y limpio el valor 
 
-function agregarAmigo(){                      // Cuando hago click llamo a esta funcion     
- let campo = document.getElementById("amigo");  // Selecciono el campo del cual quiero obtener el contenido
- let amigo = campo.value.trim();                      // Obtengo y limpio el valor 
- 
-if(amigo === "")
-    {
-    alert("Por favor, inserte un nombre.");
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre.");
+        return;
     }
-else
-{
+
     amigos.push(amigo);                        // Envio el amigo al Array
-    
+
     campo.value = "";                         // Vacio el campo 
-    
 
     actualizarListaAmigos();
-    //console.log(amigos);
-    
+
 }
-}
-function actualizarListaAmigos(){                             // Actualizo la lista
+campo.focus();
+
+function actualizarListaAmigos() {                             // Actualizo la lista
 
     let lista = document.getElementById("listaAmigos");       // selecionamos la lista donde vamos a poner a los amigos
     lista.innerHTML = "";                                     // la vaciamos antes de poner nuevos amigos
@@ -34,11 +29,27 @@ function actualizarListaAmigos(){                             // Actualizo la li
         let li = document.createElement("li");               // por cada  amigo creamos un contenido
         li.textContent = amigos[i];                          // el amigo va a el contenido
         lista.appendChild(li);                                // el contenido se agrega a la lista
-        
+
     }
-    /*amigos.forEach(amigo => {
-    let li = document.createElement("li");
-    li.textContent = amigo;
-    lista.appendChild(li);
-    });*/
 }
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la Lista ");
+        return;
+    }
+    else {
+        let aleatorio = Math.floor(Math.random() * amigos.length);
+        let posicion = amigos[aleatorio];
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = "";
+        resultado.innerText = posicion;
+
+    }
+}
+
+
+
+
+
+
